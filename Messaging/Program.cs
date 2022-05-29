4d383935dc63b2537e0dda25428b06358264fae2
Bramcoen <coenders123@gmail.com>
2022-05-29 23:07:18 +0200

    Fixed issue prometheus config

--- Messaging/Program.cs
+++ Messaging/Program.cs
@@ -5,6 +5,10 @@ using Prometheus;
 using StorageInterfaces;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options => {
+    options.AddPolicy("default", build => build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader());
+    //Check should be handled by the gateway
+});
 builder.Services.AddHostedService<NewMessageWorker>();
 builder.Services.AddHostedService<UserWorker>();
 builder.Services.AddSingleton<IUserStorage, UserStorage>();
@@ -14,6 +18,7 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 app.UseRouting();
+app.UseCors();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapMetrics();
